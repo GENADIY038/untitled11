5f07e70c179929e914ea4e20924c4e7d0d1fb909
Daniil <potekhindaniil@mail.ru>
2021-01-11 17:07:19 +0700

    commit

--- src/View.kt
+++ src/View.kt
@@ -1,11 +1,16 @@
 class View {
     var CornField: CornField = CornField()
+    var Culture: Culture = Culture()
 
     fun show(){
         println("У вас есть поле размером ${CornField.area} с культурой ${CornField.currentCulture.name}")
     }
+    fun selectCulture(Culture: Culture){
+            println("Вы выбрали ${CornField.currentCulture.name}")
+    }
 
-    fun selectCulture(){
+    fun sell(x: Int): Float{
+        return x * CornField.area
 
     }
 }
